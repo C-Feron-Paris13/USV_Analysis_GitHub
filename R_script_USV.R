dea90b2f782388fe81d7389c271b88857c7f40b1
C-Feron-Paris13 <christophe.feron@univ-paris13.fr>
2026-09-22 17:33:22 +0200

    Une ligne supplémentaire mise en commentaire

--- R_script_USV.R
+++ R_script_USV.R
@@ -6,7 +6,7 @@ setwd("./usv_data") #code ajousté
 
 fichiers<-list.files() #les fichiers du répertoire courant.
 
-list.files(pattern="*_0.csv")
+# list.files(pattern="*_0.csv") J'ai commenté cette ligne 
 
 # fichiers[1]
 # 
